f04eb04cbaa0c69b733e6f59015aa42b96f08a92
Ahmed Aly <ahmaly01@gmail.com>
2016-04-22 05:27:14 -0400

    behavior is necessary

--- js/directives.js
+++ js/directives.js
@@ -17,13 +17,6 @@ angular.module("directives", ['ngRoute'])
 		    scrollTop: $("#about").offset().top
 		}, 1000);
 	}])
-	.controller('homeSectionsCtrl', ['$scope', '$sce',function($scope, $sce){
-		console.log('home');	
-		
-		$('html, body').animate({
-		    scrollTop: $("#home").offset().top
-		}, 1000);
-	}])
 
 	.controller('gameboyCtrl', ['$scope', '$sce',function($scope, $sce){
 		$scope.parts= [
@@ -77,7 +70,6 @@ angular.module("directives", ['ngRoute'])
                 .when('/digitalArt',{controller:"digitalArtCtrl", templateUrl:'views/digitalArt.html'})
                 .when('/',{controller:"mainCtrl", templateUrl:'views/mainSections.html'})
                 .when('/about',{controller:"aboutSectionsCtrl", templateUrl:'views/mainSections.html'})
-                .when('/home',{controller:"homeSectionsCtrl", templateUrl:'views/mainSections.html'})
                 .otherwise({redirectTo:'/'});
                 ;
      }]);
